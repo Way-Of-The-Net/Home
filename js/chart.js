1e59903aa46aa3f204555a53a36827d8d7267a9d
David Napier <davidnapier@wayofthenet.com>
2021-07-15 11:41:24 -0400

    Add markers on chart for Latest Trades

--- js/chart.js
+++ js/chart.js
@@ -15,48 +15,78 @@ const lineSeries = chart.addAreaSeries({
 
 // TODO: Gather this data from database stored at Steve's
 lineSeries.setData([
-    { time: '2019-04-11', value: 80.01 },
-    { time: '2019-04-12', value: 96.63 },
-    { time: '2019-04-13', value: 76.64 },
-    { time: '2019-04-14', value: 81.89 },
-    { time: '2019-04-15', value: 74.43 },
-    { time: '2019-04-16', value: 80.01 },
-    { time: '2019-04-17', value: 96.63 },
-    { time: '2019-04-18', value: 76.64 },
-    { time: '2019-04-19', value: 81.89 },
-    { time: '2019-04-20', value: 74.43 },
+    { time: '2021-07-11', value: 80.01 },
+    { time: '2021-07-12', value: 96.63 },
+    { time: '2021-07-13', value: 76.64 },
+    { time: '2021-07-14', value: 81.89 },
+    { time: '2021-07-15', value: 74.43 },
+    { time: '2021-07-16', value: 80.01 },
+    { time: '2021-07-17', value: 96.63 },
+    { time: '2021-07-18', value: 76.64 },
+    { time: '2021-07-19', value: 81.89 },
+    { time: '2021-07-20', value: 74.43 },
 ]);
 
-lineSeries.setMarkers([
-    {
-        time: '2019-04-09',
-        position: 'aboveBar',
-        color: 'black',
-        shape: 'arrowUp',
-    },
-]);
+let xhr = new XMLHttpRequest();
+xhr.open("GET", "https://trade.wayofthe.net", false)
 
-const priceLine = lineSeries.createPriceLine({
-    price: 80.0,
-    color: 'green',
-    lineWidth: 2,
-    lineStyle: LightweightCharts.LineStyle.Dotted,
-    axisLabelVisible: true,
-    title: 'P/L 500',
-});
+let chartFormattedOrders = []
 
-priceLine.applyOptions({
-    autosize: true,
-    price: 90.0,
-    color: 'red',
-    lineWidth: 3,
-    lineStyle: LightweightCharts.LineStyle.Dashed,
-    axisLabelVisible: false,
-    title: 'P/L 600',
-    timeScale: {
-        rightOffset: 12,
-        barSpacing: 3,
-        fixLeftEdge: true,
+try {
+    xhr.send()
+    if (xhr.status != 200) {
+        alert(`Error ${xhr.status}: ${xhr.statusText}`);
+    } else {
+        let resp = JSON.parse(xhr.response);
+        let s, p, c = ''
+        for (let x = 0; x < resp.length; x++){
+            if (resp[x].order_type == 'BUY') {
+                s = 'arrowUp';
+                p = 'aboveBar';
+                c = 'green';
+            } else {
+                s = 'arrowDown';
+                p = 'belowBar';
+                c = 'red';
+            }
+            let obj = {
+                time: resp[x].time.slice(0, 10),
+                position: p,
+                color: c,
+                shape: s,
+                text: resp[x].quantity.toString()
+            }
+            chartFormattedOrders.push(obj)
+        }
+        console.log(chartFormattedOrders)
+        lineSeries.setMarkers(chartFormattedOrders)
     }
-});
+} catch(err) {
+    alert("Request Failed")
+}
+
+// const priceLine = lineSeries.createPriceLine({
+//     price: 80.0,
+//     color: 'green',
+//     lineWidth: 2,
+//     lineStyle: LightweightCharts.LineStyle.Dotted,
+//     axisLabelVisible: true,
+//     title: 'P/L 500',
+// });
+//
+// priceLine.applyOptions({
+//     autosize: true,
+//     price: 90.0,
+//     color: 'red',
+//     lineWidth: 3,
+//     lineStyle: LightweightCharts.LineStyle.Dashed,
+//     axisLabelVisible: false,
+//     title: 'P/L 600',
+//     timeScale: {
+//         rightOffset: 12,
+//         barSpacing: 3,
+//         fixLeftEdge: true,
+//     }
+// });
+
 chart.timeScale().fitContent()
